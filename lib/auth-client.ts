43d916dd473719d620f798991db621f65fcbbc27
notlongfen <phannguyenhoanglong@gmail.com>
2026-01-01 15:51:30 +0700

    feat(auth): add betterauth

---
+++ lib/auth-client.ts
@@ -0,0 +1,17 @@
+import { createAuthClient } from "better-auth/client";
+import { inferAdditionalFields } from "better-auth/client/plugins";
+
+export const authClient = createAuthClient({
+	/** The base URL of the server (optional if you're using the same domain) */
+	baseURL: "http://localhost:3000",
+	plugins: [
+		inferAdditionalFields({
+			user: {
+				role: {
+					type: "string",
+					required: true,
+				},
+			},
+		}),
+	],
+});
---
+++ lib/auth.ts
@@ -0,0 +1,23 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+import { multiSession } from "better-auth/plugins";
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        },
+    },
+    plugins: [
+        multiSession({
+           maximumSessions: 2,
+        }),
+    ]
+});
+
+export { auth as default };
