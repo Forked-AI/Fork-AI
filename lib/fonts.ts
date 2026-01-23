d9584f51075c0f27b76f1abac1fbb41d70c5d75a
notlongfen <phannguyenhoanglong@gmail.com>
2026-01-24 00:27:01 +0700

    chore(font): update font

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,4 +1,7 @@
+import { GeistSans } from "geist/font/sans";
 import { Fraunces, Manrope } from "next/font/google";
+
+export const geist = GeistSans;
 
 export const fraunces = Fraunces({
 	subsets: ["latin"],
